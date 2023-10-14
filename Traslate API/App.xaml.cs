ec9fb284a469d32ac6669f500e2f5d9105200bd0
Gurox <93458769+GuroxDG@users.noreply.github.com>
2023-10-14 00:08:46 -0500

    initial Commit

---
+++ Traslate API/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Traslate_API
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Traslate API/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Traslate_API
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
